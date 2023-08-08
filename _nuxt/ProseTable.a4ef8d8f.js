216cbc07447eb715e1da4d62590acd12adb8b07c
Oleksandr Gituliar <oleksandr@gituliar.net>
2023-08-08 22:12:53 +0200

    Updates

--- _nuxt/ProseTable.a4ef8d8f.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,c as t,U as s}from"./entry.7a386293.js";const a={};function c(e,n){return r(),t("table",null,[s(e.$slots,"default")])}const _=o(a,[["render",c]]);export{_ as default};
